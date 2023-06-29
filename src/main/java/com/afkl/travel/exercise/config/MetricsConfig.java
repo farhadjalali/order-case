773e64c7c7e9101f360bc8de52b40cf9c6f01542
Farhad <farhad@info.nl>
2023-06-29 12:04:02 +0200

    fix runtime errors

--- src/main/java/com/afkl/travel/exercise/config/MetricsConfig.java
+++ src/main/java/com/afkl/travel/exercise/config/MetricsConfig.java
@@ -3,7 +3,6 @@ package com.afkl.travel.exercise.config;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
-import io.prometheus.client.CollectorRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +11,6 @@ public class MetricsConfig {
 
     @Bean
     public MeterRegistry meterRegistry() {
-        CollectorRegistry collectorRegistry = new CollectorRegistry();
-        return new PrometheusMeterRegistry((PrometheusConfig) collectorRegistry);
+        return new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
     }
 }
